3db8cebebb562fb14042ea98a630902189ac4415
RoyNajarian <roynajarian@gmail.com>
2026-02-25 16:23:13 +0100

    Création des différentes pages et des routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Accueil from "./pages/Accueil";
+import Auth from "./pages/Auth";
+import Profil from "./pages/Profil";
+import Immersive from "./pages/Immersive";
+import Jeu from "./pages/Jeu";
+import Reservation from "./pages/Reservation";
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Routes>
+      <Route index element={<Accueil />} />
 
-export default App
+      <Route path="auth" element={<Auth />} />
+      <Route path="profil" element={<Profil />} />
+      <Route path="immersive" element={<Immersive />} />
+      <Route path="jeu" element={<Jeu />} />
+      <Route path="reservation" element={<Reservation />} />
+
+    </Routes>
+  );
+}
