2425d72d695350c1de0540f4e7f2249fefd97746
RoyNajarian <roynajarian@gmail.com>
2026-02-27 16:06:46 +0100

    Ajustement et ajout de la lumière

--- src/components/Labyrinthe.js
+++ src/components/Labyrinthe.js
@@ -8,7 +8,7 @@ export const mapLevel1 = [
     [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
     [1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
-    [1, 2, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 3, 1],
+    [2, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 3],
     [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
     [1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
@@ -20,14 +20,13 @@ export const mapLevel1 = [
 
 export const construction = (scene) => {
     const wall = new THREE.BoxGeometry(1, 1, 1);
-    const wallMaterial = new THREE.MeshBasicMaterial({ color: 0x888888 });
+    const wallMaterial = new THREE.MeshLambertMaterial({ color: 0x888888 });
 
     const ground = new THREE.BoxGeometry(1, 0.1, 1);
-    const groundMaterial = new THREE.MeshBasicMaterial({ color: 0x333333 });
+    const groundMaterial = new THREE.MeshLambertMaterial({ color: 0x333333 });
 
-    const startMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const endMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-
+    const startMaterial = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+    const endMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000 });
 
     for (let i = 0; i < mapLevel1.length; i++) {
         for (let j = 0; j < mapLevel1[i].length; j++) {
@@ -36,6 +35,7 @@ export const construction = (scene) => {
             if (numCase === 1) {
                 const wallMesh = new THREE.Mesh(wall, wallMaterial);
                 wallMesh.position.set(j, 0.5, i);
+
                 scene.add(wallMesh);
             }
 
@@ -50,6 +50,7 @@ export const construction = (scene) => {
 
             const groundMesh = new THREE.Mesh(ground, specialColor);
             groundMesh.position.set(j, 0, i);
+
             scene.add(groundMesh);
         }
     }
