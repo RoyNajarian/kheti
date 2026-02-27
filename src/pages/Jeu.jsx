d2f38af91417130c6b22752b50858a3f38ee2f9c
RoyNajarian <roynajarian@gmail.com>
2026-02-27 16:06:46 +0100

    Ajustement et ajout de la lumière

--- src/pages/Jeu.jsx
+++ src/pages/Jeu.jsx
@@ -14,11 +14,19 @@ export const Jeu = () => {
         const scene = new THREE.Scene();
         construction(scene);
 
+        // Lumière
+        const light = new THREE.DirectionalLight(0xffffff, 2);
+        light.position.set(13, 20, 7);
+        scene.add(light);
+
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+        scene.add(ambientLight);
+
         // Caméra
         const camera = new THREE.PerspectiveCamera(60, container.clientWidth / container.clientHeight, 0.1, 1000);
-        camera.position.set(13, 18, 9);
-        camera.up.set(0, 0, -2);
-        camera.lookAt(13, 2, 7);
+        camera.position.set(13, 18, 7);
+        camera.up.set(0, 0, -1);
+        camera.lookAt(13, 0, 6.5);
 
         // Renderer
         const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
