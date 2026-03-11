ed09443379ea619e0e8f8f6bf130572fafd2c851
Chalinger <victor.le.claire@icloud.com>
2026-03-11 11:30:18 +0100

    Début de la page d'acceuil. Insertion d'images d'arrière plan de l'accueil et du logo Kheti.

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,8 +1,24 @@
 const Accueil = () => {
     return (
-        <div>
-            <h1>Accueil</h1>
-        </div>
+        <>test
+            {/* <section className="accueil">
+                <header className="accueil-header">
+                <nav>
+                    <ul>
+                        <li><a href="/">Réserver</a></li>
+                        <li><a href="/">Connexion</a></li>
+                    </ul>
+                </nav>
+            </header>
+                <div className="accueil-content" style={{ border: "1px solid red" }}>
+                    <div className="accueil-main" style={{ border: "1px solid blue" }}>
+                        <img src="/public/images/kheti-logo.png" alt="Kheto Logo" style={{ border: "1px solid black" }} />
+                        <button className="accueil-trailer-btn" >Voir le trailer</button>
+                    </div>
+                    <div className="accueil-pharaon" style={{ border: "1px solid white" }}></div>
+                </div>
+            </section> */}
+        </>
     );
 }
 
