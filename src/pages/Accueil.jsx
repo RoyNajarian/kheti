5895987f0b4537923f02431ddff71235c9f8e0e7
Chalinger <victor.le.claire@icloud.com>
2026-03-13 12:25:37 +0100

    Hero de la page d'accueil 80%.

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -16,8 +16,20 @@ const Accueil = () => {
                     </div>
 
                     <img src="/public/images/pharaon.png" alt="Pharaon" className="accueil-pharaon" />
-
+                    <div className="accueil-bd">
+                        <img src="/public/images/b&m_le-mystere-de-le-grande-pyramide.png" alt="B&M" />
+                        <img src="/public/images/tintin_les-cigares-du-pharaon.png" alt="Tintin" />
+                        <img src="/public/images/asterix-et-cleopatre.png" alt="Asterix" />
+                    </div>
+                    <a className="accueil-scroll-action">
+                        <span>Voir plus</span>
+                        <svg width="18" height="38" viewBox="0 0 65 130" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M44.976 89C44.976 89.5523 44.5282 90 43.976 90H20.976C20.4237 90 19.976 89.5523 19.976 89V1C19.976 0.447718 20.4237 0 20.976 0H43.976C44.5282 0 44.976 0.447715 44.976 1V89Z" fill="white" />
+                            <path d="M32.476 130L64.9519 77.5H0L32.476 130Z" fill="white" />
+                        </svg>
+                    </a>
                 </div>
+
             </section>
         </>
     );
